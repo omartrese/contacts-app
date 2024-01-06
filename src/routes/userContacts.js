83cad91e9802312a2f7da8c6fc3bc77230b77d0f
omartrese <omarlayachi13@gmail.com>
2024-01-06 20:27:01 +0100

    contacts delete

--- src/routes/userContacts.js
+++ src/routes/userContacts.js
@@ -4,39 +4,65 @@ const router = express.Router();
 const sql = require('./../db');
 let user; 
 
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));
+
+
 router.get('/contacts', (req, res) => {  
-    
-    if (currentUser) {
-      user = currentUser();
-      console.log("CURRENT USER= " + user.username);
-      res.redirect(`/contacts/${user.username}`);
-    } else {
-      res.redirect('/auth/login');
-      console.log("algo no va por aquí my broder");
-    }
-  });
+  
+  if (currentUser) {
+    user = currentUser();
+    console.log("CURRENT USER= " + user.username);
+    res.redirect(`/contacts/${user.username}`);
+  } else {
+    res.redirect('/auth/login');
+    console.log("algo no va por aquí my broder");
+  }
+});
+
 
 router.get('/contacts/:user', (req, res) => {
   console.log(user);
   // TODO --> Start doing the freaking contacts backend to end t his shitty project, I wanna do a Fucking videogame with Unity and Blender
   
-  sql.query(`SELECT * FROM contacts WHERE userOwner = '${user.username}'`, (err, results) => {
+  sql.query(`SELECT * FROM contacts WHERE userOwner = '${user.username}'`, async (err, results) => {
     if(err) return console.log(err);
-
-    res.render('contacts', {username: user.username, contacts: results});
-    })
+    
+    await res.render('contacts', {username: user.username, contacts: results});
+  })
 });
 
+
+router.get('/contacts/:user/add', async (req, res) => {
+  await res.render('add', {username: user.username});
+});
+
+
 router.post('/contacts/:user', (req, res) => {
-  console.log(req.body)
-  res.redirect(`/contacts/${currentUser().username}`);
+  //console.log(req.body)
+  let contactName = req.body.name;
+  let contactTlf = req.body.tlf;
   // TODO --> make this post request adds the contacts to the database
-})
 
-router.get('/contacts/:user/add', (req, res) => {
-  res.render('add', {username: currentUser().username});
-})
+  sql.query('INSERT INTO contacts SET ?', {name: contactName, tlfnum: contactTlf, userOwner: user.username}, (error) => {
+    if(error) return res.status(500, {Error: 'not valid data'}); 
+    console.log('current user is: ', user);
+    
+  });
+  res.redirect(`/contacts`);
+});
 
+
+router.post('/contacts/delete/:id', (req, res) => {
+  const contactId = req.params.id;
+  console.log('contact ID: ', contactId);
+  
+  sql.query(`DELETE FROM contacts WHERE id = ${contactId}`, (error) => {
+    if (error) return console.log(error);
+  });
+
+  res.redirect(`/contacts`);
+});
 
 
 module.exports = router;
