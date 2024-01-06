c09ea4d55934f96f8bc894974146a7e9930598ba
omartrese <omarlayachi13@gmail.com>
2024-01-06 20:47:39 +0100

    finished app
    the basic tasks the app would do are finished

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,21 @@
-/*
-MAKE SCRIPT TO CREATE DATABASE AUTOMATICALLY FOR THE USER THAT WAN'T TO MAKE FEATURES TO
-THIS PROJECT
-*/
 
-SELECT * from users;
+CREATE DATABASE IF NOT EXISTS contactsapp;
+USE contactsapp;
+
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    tlfnum VARCHAR(20) NOT NULL,
+    userOwner VARCHAR(255) NOT NULL
+); 
+
+
+-- TO TEST
+
+-- INSERT INTO users (username, password)
+-- VALUES ('test', 'testPass');
